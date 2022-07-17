bce4ed502707a2c932c63ca1c14f25b300d466f1
spatel210 <101787142+spatel210@users.noreply.github.com>
2022-07-17 22:14:04 +0530

    Changes network connection

--- Assets/Photon/PhotonUnityNetworking/Demos/DemoAsteroids/Scripts/AsteroidsGame.cs
+++ Assets/Photon/PhotonUnityNetworking/Demos/DemoAsteroids/Scripts/AsteroidsGame.cs
@@ -4,6 +4,7 @@ namespace Photon.Pun.Demo.Asteroids
 {
     public class AsteroidsGame
     {
+        
         public const float ASTEROIDS_MIN_SPAWN_TIME = 5.0f;
         public const float ASTEROIDS_MAX_SPAWN_TIME = 10.0f;
 
@@ -31,5 +32,7 @@ namespace Photon.Pun.Demo.Asteroids
 
             return Color.black;
         }
+        
     }
+    
 }
